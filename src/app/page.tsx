11b118612e6986f22169f87f456df570607615b3
dominique savio <143187774+saviomds@users.noreply.github.com>
2024-02-08 21:05:43 +0200

    update page.tsx on router login (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <span>hello eshuri</span>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-600">
+      <h1 className="text-4xl sm:text-6xl lg:text-9xl text-gray-400">
+        <strong className="text-gray-100">eShuri</strong> web
+      </h1>
+      <div className="text-center">
+        <Link href="./loginUser">
+          <button className="btn btn-md mt-10 bg-gray-800 text-gray-50 rounded-full px-6 py-3">
+            Responsive
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
